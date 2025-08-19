26fd21f236196e6b323253de5cb423b435b0fccf
Jominjong <whalswhd789@naver.com>
2025-08-19 14:22:22 +0900

    첫번쩨 테스트

---
+++ cookhub/src/test/java/com/cookhub/app/CookhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookhub.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
