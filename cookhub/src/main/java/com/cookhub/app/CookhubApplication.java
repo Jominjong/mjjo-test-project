46171f5ac0609fd2b7ab804aa21f2723e4af21da
Jominjong <whalswhd789@naver.com>
2025-08-19 14:22:22 +0900

    첫번쩨 테스트

---
+++ cookhub/src/main/java/com/cookhub/app/CookhubApplication.java
@@ -0,0 +1,13 @@
+package com.cookhub.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookhubApplication.class, args);
+	}
+
+}
